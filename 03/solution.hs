36a5f762839c03a1ac77f808c0a17ad84f6b9375
treythomas123 <treythomas123@gmail.com>
2015-04-15 12:47:04 -0600

    Fix problem 3 to be 1-indexed

--- 03/solution.hs
+++ 03/solution.hs
@@ -8,8 +8,6 @@
 -- 'e'
 
 elementAt :: [a] -> Int -> a
-elementAt [] i = error "empty array"
-elementAt (x:xs) i
-    | i == 0         = x
-    | i <= length xs = elementAt xs (i-1)
-    | otherwise      = error "index out of bounds"
+elementAt (x:_) 1 = x
+elementAt (x:xs) i =  elementAt xs (i-1)
+elementAt _ _ = error "index out of bounds"
