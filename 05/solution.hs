b9b3cb0467aedd73d8d2dba6e9dd8407d8a3f876
treythomas123 <treythomas123@gmail.com>
2015-04-15 12:53:33 -0600

    Add solution to problem 5

---
+++ 05/solution.hs
@@ -0,0 +1,10 @@
+-- Reverse a list.
+--
+-- Prelude> myReverse "A man, a plan, a canal, panama!"
+-- "!amanap ,lanac a ,nalp a ,nam A"
+-- Prelude> myReverse [1,2,3,4]
+-- [4,3,2,1]
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
