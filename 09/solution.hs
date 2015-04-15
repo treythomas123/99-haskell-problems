4629158066096ff042f391c88322742f479b566e
treythomas123 <treythomas123@gmail.com>
2015-04-15 17:21:33 -0600

    Add solution to problem 9

---
+++ 09/solution.hs
@@ -0,0 +1,15 @@
+-- Pack consecutive duplicates of list elements into sublists. If a list
+-- contains repeated elements they should be placed in separate sublists.
+--
+-- Example:
+--
+-- *Main> pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a',
+--              'a', 'd', 'e', 'e', 'e', 'e']
+--              ["aaaa","b","cc","aa","d","eeee"]
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack [x] = [[x]]
+pack (x:ys) = (x:dupes) : pack rest
+    where dupes = takeWhile (== x) ys
+          rest  = dropWhile (== x) ys
